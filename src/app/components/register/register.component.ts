4948775da5927f8376c69e8ec17048014bb6b611
AlasDev <saladavide003@gmail.com>
2025-05-16 10:30:25 +0200

    Edit fixed register component

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -6,30 +6,36 @@ import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
-    imports: [
-        ReactiveFormsModule
-    ],
+  standalone: false,
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  registerForm: FormGroup;
+
   constructor(private fb: FormBuilder,
               private http: HttpClient,
               private router: Router,
-              private authService: AuthService) {}
+              private authService: AuthService) {
 
-  registerForm = new FormGroup({
-    name: new FormControl('', [Validators.required]),
-    surname: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required])
-  });
+    this.registerForm = this.fb.group({
+      name: new FormControl('', [Validators.required]),
+      surname: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+    });
+  }
 
   onSubmit() {
     if (!this.registerForm.valid) {
       console.error("form not valid")
       return;
     }
-    this.authService.login(this.registerForm)
+    this.authService.register(
+      this.registerForm.value.name,
+      this.registerForm.value.surname,
+      this.registerForm.value.email,
+      this.registerForm.value.password)
   }
 }
