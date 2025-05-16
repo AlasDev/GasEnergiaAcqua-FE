538977bfe1b3b373ecd4f3d652f14512537cb381
AlasDev <saladavide003@gmail.com>
2025-05-16 10:30:42 +0200

    Edit fixed login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,34 +1,34 @@
 import {Component} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [
-    ReactiveFormsModule
-  ],
+  standalone: false,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  loginForm: FormGroup;
 
   constructor(private fb: FormBuilder,
               private http: HttpClient,
               private router: Router,
-              private authService: AuthService) {}
-
-  loginForm = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required])
-  });
+              private authService: AuthService) {
+    this.loginForm = this.fb.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+    });
+  }
 
   onSubmit() {
     if (!this.loginForm.valid) {
       console.error("form not valid")
       return;
     }
-    this.authService.login(this.loginForm)
+    this.authService.login(this.loginForm.value.email, this.loginForm.value.password);
   }
 }
