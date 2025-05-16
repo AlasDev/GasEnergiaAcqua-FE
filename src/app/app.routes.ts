57c78e75487bd44cf098a6fe105f9e40808dcd36
AlasDev <saladavide003@gmail.com>
2025-05-16 17:08:36 +0200

    Edit added new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,15 @@ import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
 import {LoginComponent} from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
+import {UserComponent} from './components/user/user.component';
+import {HomePageComponent} from './components/home-page/home-page.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent},
-  { path: 'register', component: RegisterComponent}
+  { path: 'register', component: RegisterComponent},
+  { path: 'homepage', component: HomePageComponent},
+  { path: 'profile', component: UserComponent},
 ];
 
 @NgModule({
--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {Router, RouterLink} from '@angular/router';
 import {AuthService} from '../../services/auth.service';
+import {UserService} from '../../services/user.service';
+import {routes} from '../../app.routes';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [
-    RouterLink
-  ],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.css'
+  standalone: false,
+  styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
-  constructor(protected authService: AuthService) {
+export class NavBarComponent implements OnInit {
+  constructor(protected authService: AuthService, protected userService: UserService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    console.log("NavBar initialized");
+    if (localStorage.getItem("token") != null) {
+      this.authService._loginStatus.next(true)
+    } else {
+      this.authService._loginStatus.next(false)
+      this.router.navigate(['login']);
+    }
   }
 }
