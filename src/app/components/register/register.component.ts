bc295d6a29d8818cc2aafcaa81a4e8df93088dc2
AlasDev <saladavide003@gmail.com>
2025-06-02 12:07:21 +0200

    Edit edited register

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {HttpClient} from '@angular/common/http';
-import {Router} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
 import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
-  standalone: false,
   templateUrl: './register.component.html',
+  standalone: false,
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
