e10e4204c31dcb478c0c4abeb3f4af87cda372d8
AlasDev <saladavide003@gmail.com>
2025-06-02 12:09:02 +0200

    Edit added token validation check on app reload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {AuthService} from './services/auth.service';
+import {ToastHandlerService} from './services/toast-handler.service';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,19 @@ import {Component, OnInit} from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+
+  constructor(protected authService: AuthService, protected toastHandler: ToastHandlerService) { }
+
+  ngOnInit(): void {
+    console.log("App initialized");
+    if (this.authService.isTokenValid()) {
+      this.toastHandler.showSuccessMessage("Token is still valid.");
+    } else {
+      this.toastHandler.showSuccessMessage("Token is invalid or expired, performing logout.");
+      this.authService.logout();
+    }
+  }
   title = 'GasEnergiaAcqua';
 }
