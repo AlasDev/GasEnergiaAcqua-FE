f88d757c8de288ad09a6fa63d131ce0c8b0b5190
AlasDev <saladavide003@gmail.com>
2025-06-02 11:45:43 +0200

    Add added home page component to contain all the info of the user's utility meters

---
+++ src/app/components/home-page/home-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page.ts',
+  templateUrl: './home-page.component.html',
+  standalone: false,
+  styleUrl: './home-page.component.css'
+})
+export class HomePageComponent {
+
+}
