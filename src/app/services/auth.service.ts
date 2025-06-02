54d2c5d0fc5a9ac763b551806b90909a78f29f6b
AlasDev <saladavide003@gmail.com>
2025-06-02 12:08:25 +0200

    Edit added new methods

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import {Router} from '@angular/router';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {LoginSuccessfulResponseMessage, ResponseMessage} from '../responses/responseMessage';
-import {BehaviorSubject} from 'rxjs';
-import {FormControl, FormGroup, ɵValue} from '@angular/forms';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {ToastHandlerService} from './toast-handler.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,29 +11,67 @@ import {FormControl, FormGroup, ɵValue} from '@angular/forms';
 export class AuthService {
 
   private baseUrl: string = 'http://localhost:8080/api/auth';
-  private $loginStatus: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  public _loginStatus: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  public loginStatus$: Observable<boolean> = this._loginStatus.asObservable();
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private http: HttpClient, private router: Router, private toastHandler: ToastHandlerService) {}
 
-  login(loginForm: FormGroup) {
-    this.http.post<LoginSuccessfulResponseMessage>(`${this.baseUrl}/login`, loginForm).subscribe({
+  login(email: string, password: string) {
+    this.http.post<LoginSuccessfulResponseMessage>(`${this.baseUrl}/login`, {email, password}).subscribe({
       next: (response) => {
         console.log("response: " + response.message);
-        if (response.token != null) this.$loginStatus.next(true);
+        if (response.token != null) {
+          localStorage.setItem('token', response.token);
+          this._loginStatus.next(true)
+          this.toastHandler.showSuccessMessage(response.message);
+        }
+        this.router.navigate(['homepage']);
       }
     })
   }
 
-  register(registerForm: FormGroup) {
-    this.http.post<ResponseMessage>(`${this.baseUrl}/register`, registerForm).subscribe({
+  register(name: string, surname: string, email: string, password: string) {
+    this.http.post<ResponseMessage>(`${this.baseUrl}/register`, {name, surname, email, password}).subscribe({
       next: (response) => {
         console.log("response: " + response.message);
+        this.toastHandler.showInfoMessage(response.message);
       }
     })
   }
 
   public logout() {
     localStorage.removeItem('token');
-    this.$loginStatus.next(false);
+    this._loginStatus.next(false);
+  }
+
+  public isLogged(): boolean {
+    if (localStorage.getItem("token") != null) {
+      this._loginStatus.next(true)
+      return true
+    } else {
+      this.logout();
+      this._loginStatus.next(false);
+      this.router.navigate(['login']);
+      return false;
+    }
+  }
+
+  public isTokenValid(): boolean {
+    if (localStorage.getItem("token") != null) {
+      const httpHeaders = this.getAuthHeader()
+      this.http.post<boolean>(`${this.baseUrl}/tokenValidation`, {headers: httpHeaders}).subscribe({
+        next: (response) => {
+          console.log("token isValid: " + response);
+          return response;
+        }
+      })
+    }
+    return false
+  }
+
+  public getAuthHeader(): HttpHeaders {
+    return new HttpHeaders({
+      Authorization: localStorage.getItem("token") as string
+    })
   }
 }
