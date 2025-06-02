c2687cdef71cbc0cf54f6f3b02fda05735fb1593
AlasDev <saladavide003@gmail.com>
2025-06-02 12:11:49 +0200

    Edit edited imports and declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,12 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {UserComponent} from './components/user/user.component';
-import {LineChartComponent} from './components/line-chart/line-chart.component';
 import {AddressComponent} from './components/address/address.component';
 import {ReadingComponent} from './components/reading/reading.component';
-import {CdkAccordion, CdkAccordionItem} from '@angular/cdk/accordion';
 import {HomePageComponent} from './components/home-page/home-page.component';
+import {UtilityMeterComponent} from './components/utility-meter/utility-meter.component';
+import {UtilityMeterAccordionComponent} from './components/utility-meter-accordion/utility-meter-accordion.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -22,16 +23,17 @@ import {HomePageComponent} from './components/home-page/home-page.component';
     RegisterComponent,
     HomePageComponent,
     UserComponent,
+    UtilityMeterComponent,
+    UtilityMeterAccordionComponent,
     AddressComponent,
-    ReadingComponent,
-
-    LineChartComponent,
+    ReadingComponent
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [provideHttpClient(withInterceptorsFromDi())],
   bootstrap: [AppComponent]
